e63030a7f7e7ea4c07bf9e1139cb32526bfdd232
Mike DAVID <figment_scorn_0q@icloud.com>
2025-01-06 18:11:24 +1000

    separate .cpp files for about and help dialogs

---
+++ src/aboutdialog.cpp
@@ -0,0 +1,73 @@
+// aboutdialog.cpp
+
+#include "aboutdialog.h"
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QPushButton>
+
+AboutDialog::AboutDialog(QWidget* parent /*= nullptr*/) : QDialog(parent) {
+    setWindowTitle("About");
+    setModal(true);
+    resize(600, 540);  // Set a reasonable default size
+
+    QVBoxLayout* layout = new QVBoxLayout(this);
+
+    QTextBrowser* htmlBrowser = new QTextBrowser;
+    htmlBrowser->setOpenExternalLinks(true);
+    htmlBrowser->setHtml(R"(
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml">
+        <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"/>
+        <title>About getinfo</title>
+        <!--<link href="style.css" rel="stylesheet" type="text/css"/>-->
+        </head>
+        <body>
+        <div id="main_content" align="center">
+        <div id="content"  align="center">
+        <p><img src="images/findfiles.png"</img></p>
+        <br/>
+        <h2><a href="http://overskys.com">Overskys.com</a></h2>
+        <br/>
+        <p align="center"><strong>Copyright &copy; 2015 Milivoj (Mike) Davidov. All rights reserved. </strong></p>
+        <br/>
+        <p align="left">
+        This software application was developed with best intentions; the author believes it functions correctly.
+        <br/>
+        However:
+        <br/>
+        <br/>
+        THIS SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESSED
+        OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+        <br/>
+        <br/>
+        IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+        DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+        OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+        USE OR OTHER DEALINGS IN THE SOFTWARE.
+        <br/>
+        <br/>
+        WARNING: This computer program is protected by copyright law and international treaties.
+        </p>
+        <!--
+        <p align="center">Favourite quote or slogan.</p>
+        <h2>H2 SubTitle #2</h2>
+        <p>Time flies when you are having fun. </p>
+        -->
+        </div>
+        </div>
+        <!--<div id="footer" align="center"><strong>Copyright &copy; Milivoj (Mike) Davidov</strong></div>-->
+        <br/>
+        </body>
+        </html>
+    )");
+
+    QPushButton* closeButton = new QPushButton("Close");
+
+    layout->addWidget(htmlBrowser);
+    layout->addWidget(closeButton);
+
+    connect(closeButton, &QPushButton::clicked, this, &QDialog::accept);
+}
---
+++ src/helpdialog.cpp
@@ -0,0 +1,36 @@
+
+#include "helpdialog.h"
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QTextBrowser>
+#include <QPushButton>
+
+
+HelpDialog::HelpDialog(QWidget* parent /*= nullptr*/) : QDialog(parent) {
+    setWindowTitle("Help");
+    setModal(true);
+    resize(600, 420);  // Set a reasonable default size
+
+    QVBoxLayout* layout = new QVBoxLayout(this);
+
+    QTextBrowser* helpBrowser = new QTextBrowser;
+    helpBrowser->setOpenExternalLinks(true);
+    helpBrowser->setHtml(R"(
+        <h2>Application Help</h2>
+        <h3>Getting Started</h3>
+        <p>Here's how to use the application:</p>
+        <ul>
+            <li>First step...</li>
+            <li>Second step...</li>
+        </ul>
+        <h3>Features</h3>
+        <p>Description of features...</p>
+    )");
+
+    QPushButton* closeButton = new QPushButton("Close");
+
+    layout->addWidget(helpBrowser);
+    layout->addWidget(closeButton);
+
+    connect(closeButton, &QPushButton::clicked, this, &QDialog::accept);
+}
